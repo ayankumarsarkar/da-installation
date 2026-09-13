7f039f6224eabaeb90a82e0f5373ffccc24a9432
Aeon <ayans5929@gmail.com>
2026-09-13 08:17:36 +0530

    Add installation script for setting up user environment and software

---
+++ install.sh
@@ -0,0 +1,31 @@
+# if sudo is not installed
+# apt install sudo
+
+# use this if usermod did not work
+# /usr/shin/usermod -aG user $user
+
+# ask for $user if none found
+
+# switch to user space
+
+# create default folders
+mkdir ~/.config/
+mkdir ~/.config/sway/
+mkdir ~/.config/falkon
+mkdir ~/.config/bluefish
+mkdir ~/.config/rofi-wayland
+mkdir ~/.config/waybar
+mkdir ~/Desktop/
+mkdir ~/Documents/
+mkdir ~/Inbox/
+mkdir ~/Pictures/
+mkdir ~/Templates/
+mkdir ~/Videos/
+
+# install software
+sudo apt install sway waybar rofi-wayland alacritty pcmanfm-qt falkon bluefish
+
+#wget <link to TutaMail> 
+#wget <link to Koofr>
+
+
