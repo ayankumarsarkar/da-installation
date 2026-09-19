621b0d00249c4de561390440efab281bdf65eff1
Aeon <ayans5929@gmail.com>
2026-09-19 21:23:39 +0530

    Add installation script for Rust with curl check

--- install-rust.sh
+++ install-rust.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
 
+check_installation_of() {
+   command -v "$1" &> /dev/null
+}
+
+if check_installation_of rustup; then
+	echo "Rust should already be installed."
+	exit 0
+fi
+
+install_rust() {
+	curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+}
+
+if check_installation_of curl; then
+	echo "Curl is installed."
+	install_rust
+else
+	echo "* Curl is not installed."
+	sudo apt install curl
+	install_rust
+	sudo apt remove curl
+	sudo apt autoremove
+fi
+
+echo "Rust version:"
+rustc --version
